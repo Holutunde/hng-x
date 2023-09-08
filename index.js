138b4661b5f1f794538fc3bad7a6c7b66e0ec154
Holutunde <Holutunde99@gmail.com>
2023-09-09 00:13:56 +0100

    more fix

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ app.use(express.json());
 // Define the endpoint
 app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
-  const utc_time = new Date().toISOString();
+  const currentUTC = new Date();
   const currentDay = new Date().toLocaleDateString("en-US", {
     weekday: "long",
   });
+
+  // Format the UTC time to include "Z" at the end
+  const utc_time = currentUTC.toISOString().slice(0, -5) + "Z";
 
   const response = {
     slack_name,
